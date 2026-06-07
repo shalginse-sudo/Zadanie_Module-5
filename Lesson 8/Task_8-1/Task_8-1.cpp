afcd358f71c599674922ce151929e845495e6067
Sergey Shalygin <shalyginse@yandex.ru>
2026-06-07 16:50:11 +0300

    Add Task_8-1,2

---
+++ Lesson 8/Task_8-1/Task_8-1.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+void remove_duplicates(std::vector<int>& input)
+{
+    std::sort(input.begin(), input.end());
+    input.erase(std::unique(input.begin(), input.end()), input.end());
+}
+
+int main()
+{
+    std::vector<int> data{ 1, 1, 2, 5, 6, 1, 2, 4 };
+    remove_duplicates(data);
+
+    for (auto v : data) {
+        std::cout << v << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
---
+++ Lesson 8/Task_8-2/Task_8-2.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <set>
+#include <list>
+#include <vector>
+#include <string>
+
+template<typename Iter>
+void print_container(Iter begin, Iter end)
+{
+    if (begin == end) {
+        return;
+    }
+
+    auto curr = begin;
+    std::cout << *curr;
+    std::advance(curr, 1);
+
+    while (curr != end) {
+        std::cout << " " << *curr;
+        std::advance(curr, 1);
+    }
+}
+
+int main()
+{
+    std::set<std::string> test_set = { "one", "two", "three", "four" };
+    print_container(test_set.begin(), test_set.end());
+    std::cout << std::endl;
+
+    std::list<std::string> test_list = { "one", "two", "three", "four" };
+    print_container(test_list.begin(), test_list.end());
+    std::cout << std::endl;
+
+    std::vector<std::string> test_vector = { "one", "two", "three", "four" };
+    print_container(test_vector.begin(), test_vector.end());
+    std::cout << std::endl;
+    return 0;
+}
